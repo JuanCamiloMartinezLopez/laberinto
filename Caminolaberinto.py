40655c5119427059405ed587ca592801dd82a555
JuanCamiloMartinezLopez <42418412+JuanCamiloMartinezLopez@users.noreply.github.com>
2019-05-27 14:05:46 -0500

    Add files via upload

---
+++ Caminolaberinto.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class  nodo(valor,hijos):
+    def __init__(self,valor,hijos=[]):
+        self.valor=valor
+        self.hijos=hijos
+
+def GenerarArbol(matriz):
+    #for i in range(0,len(matriz))
+
+
+
+if __init__=="__main__":
+    matriz=[[1,2],[3,2],[4,5]]
+    print len(matriz)
+    print len(matriz[0])
